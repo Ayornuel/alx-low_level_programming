882c3090af411a85817790d98035d80bb20b18ec
root <ayomide.olabisi@outlook.comgit config --global user.name Ayornuelgit config user.email ayomide.olabisi@outlook.com>
2021-08-26 11:37:02 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints a string to the standard error file
+ * Return: 1
+ */
+int main(void)
+{
+char err_msg[] =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(err_msg, 1, sizeof(err_msg) - 1, stderr);
+return (1);
+}
