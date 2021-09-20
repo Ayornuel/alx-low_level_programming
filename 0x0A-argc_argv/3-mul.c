d546ba49eca482e744cc03c1dd681ab79cb90947
Ayornuel <88332379+Ayornuel@users.noreply.github.com>
2021-09-20 14:22:25 -0700

    Corrected

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-  int result;
-  (void) argv;
+int result;
+(void) argv;
 
-  if (argc != 3)
-    {
-      printf("Error\n");
-    }
-  else
-    {
-      result = ((atoi(argv[1])) * (atoi(argv[2])));
-      printf("%d\n", result);
-    }
-  return (0);
+if (argc != 3)
+{
+printf("Error\n");
+}
+else
+{
+result = ((atoi(argv[1])) * (atoi(argv[2])));
+printf("%d\n", result);
+}
+return (0);
 }
