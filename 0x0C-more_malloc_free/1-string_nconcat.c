85c30822ed4df39444f0ecd3590c85820ebc31e9
Ayornuel <88332379+Ayornuel@users.noreply.github.com>
2021-09-24 05:29:35 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,42 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1:stirng 1
- * @s2:string 2
- * @n: n bytesofstring 2
- * Return: pointer
+ * string_nconcat - the returned pointer shall point to a newly allocated
+ * space in memory, which contains s1, followe by the firs n bytes of s2,
+ * and null terminated
+ * Description: if n is greater or equal to the length of s2 then
+ * use the entire string s2
+ * Return: If NULL is passed, treat it as an empty string
+ * @s1: first string
+ * @s2: second string
+ * @n: size
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *ptr, *empt;
-  unsigned int i, k;
+	char *s;
+	unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
-  empt = "";
-  if (s1 == NULL)
-    s1 = empt;
-  if (s2 == NULL)
-    s2 = empt;
-  for (i = 0; s1[i] != '\0'; i++)
-    ;
-  ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
-  if (ptr == NULL)
-    return (NULL);
-  for (i = 0; s1[i] != '\0'; i++)
-    ptr[i] = s1[i];
-  for (k = 0; s2[k] != '\0' && k < n; k++, i++)
-    ptr[i] = s2[k];
-  ptr[i] = '\0';
-  return (ptr);
+	while (s1 && s1[length1])
+		length1++;
+	while (s2 && s2[length2])
+		length2++;
+	if (n < length2)
+		s = malloc(sizeof(char) * (length1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (length1 + length2 + 1));
+	if (!s)
+		return (NULL);
+	while (i < length1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	while (n < length2 && i < (length1 + n))
+		s[i++] = s2[j++];
+	while (n >= length2 && i < (length1 + length2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
